1c04ec4294c0daf800a01d0883a7e5ee09a65e5a
Roberto Reale <roberto@reale.me>
2021-02-03 16:15:45 +0100

    Save subsets in example.py

--- example.py
+++ example.py
@@ -5,12 +5,20 @@
 # Consumes open data about Covid-19 in Italy
 
 import sys
+import uuid
+from anguis.gdrive import AnguisGdrive
 from Covid19Vaccini import Covid19Vaccini
 
 df = Covid19Vaccini.Covid19Vaccini().somministrazioni_vaccini_latest
 
 if len(sys.argv) > 1:
     area = sys.argv[1]
-    print(df[df['area'] == area])
+    subset = df[df['area'] == area]
 else:
-    print(df)
+    subset = df
+
+# Google Driver API must be enabled
+cache = AnguisGdrive()
+cache[str(uuid.uuid4())] = subset
+
+print(subset)
