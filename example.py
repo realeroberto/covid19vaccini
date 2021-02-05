4b0158705fa7c71349ef63417a63ab32d521e175
Roberto Reale <roberto@reale.me>
2021-02-05 03:40:00 +0100

    Use Anguis as key-store layer.

--- example.py
+++ example.py
@@ -4,21 +4,26 @@
 #
 # Consumes open data about Covid-19 in Italy
 
-import sys
 import uuid
-from anguis.gdrive import AnguisGdrive
+import yaql
+from datetime import date
 from covid19vaccini import Covid19Vaccini
 
-df = Covid19Vaccini.Covid19Vaccini().somministrazioni_vaccini_latest
+# Uses https://github.com/reale/anguis as key-store layer
+from anguis.s3 import AnguisS3
 
-if len(sys.argv) > 1:
-    area = sys.argv[1]
-    subset = df[df['area'] == area]
-else:
-    subset = df
+BUCKET = '69303b61-a751-44f6-8b23-eb8f2ace3ccb'
+store = AnguisS3(BUCKET, create=True)
 
-# Google Driver API must be enabled
-cache = AnguisGdrive()
-cache[str(uuid.uuid4())] = subset
+when = date.isoformat(date.today())
 
-print(subset)
+try:
+    # Retrieve data from S3 store
+    df = store[when]
+except:
+    # Retrieve data from data source
+    df = Covid19Vaccini.Covid19Vaccini().somministrazioni_vaccini_latest
+    store[when] = df
+
+# Print data
+print(df)
